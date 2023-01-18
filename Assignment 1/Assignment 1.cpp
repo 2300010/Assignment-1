35e6877995d92d9c5b7271fd369b3d2f35f21910
samab <samab@SAMAB>
2023-01-17 22:52:25 -0500

    Structure du main créée Exercice 2.3 fonctionnel

--- Assignment 1/Assignment 1.cpp
+++ Assignment 1/Assignment 1.cpp
@@ -3,19 +3,122 @@
 
 #include <iostream>
 #include "Exercice_2.h"
+#include "Assignment 1.h"
 using namespace std;
 
 int main()
 {
-    //Declare objects of the exercices classes
-    Exercice_2 ex2;
+	//Declare objects of the exercices classes
+	Exercice_2 ex2;
 
-    int userEntryFloors;
+	//Declare variables for user entry
+	char userEntryChoice;
+	
+	try
+	{
+		cout << "Please enter the exercice number to test it:" << endl;
 
-    cout << "Please enter the number of floors: ";
-    cin >> userEntryFloors;
+		do 
+		{
+			cout << "Exercice 1" << endl;
+			cout << "Exercice 2" << endl;
+			cout << "Exercice 3" << endl;
+			cout << "Exercice 4" << endl;
+			cout << "Exercice 5" << endl;
+			cout << "Exercice 6" << endl;
+			cout << "Exercice 7" << endl;
+			cout << "Exercice 8" << endl;
+			cout << "Exercice 9" << endl;
+			cout << "To quit, enter 0: ";
+			cin >> userEntryChoice;
 
-    cout << "The building is " << ex2.BuildingHeightCalculator(userEntryFloors) << " meters.";
+			switch (userEntryChoice)
+			{
+				case '1':
+				{
+					cout << "Exercice 1";
+				}
+					break;
+				case '2':
+				{
+					//Declare variables
+					int userEntryFloors;
+					float userEntryPressure;
+
+					//Ask user to enter number of floors for the building
+					cout << "\nPlease enter the number of floors: ";
+					cin >> userEntryFloors;
+
+					//Print result of the calculation
+					cout << "\nA building of " << userEntryFloors << " floors is " 
+						<< ex2.BuildingHeightCalculator(userEntryFloors) << " meters.\n";
+
+					//Ask user to enter number of floors for the building
+					cout << "\nPlease enter the pressure in kPa: ";
+					cin >> userEntryPressure;
+
+					//Print result of the conversion
+					cout << "\nA pressure of " << userEntryPressure << " kPA is equal to " 
+						<< ex2.AtmospherePressureConverter(userEntryPressure) << " atm.\n\n";
+
+					system("pause");
+				}
+					break;
+				case '3':
+				{
+
+				}
+					break;
+				case '4':
+				{
+
+				}
+					break;
+				case '5':
+				{
+
+				}
+					break;
+				case '6':
+				{
+
+				}
+					break;
+				case '7':
+				{
+
+				}
+					break;
+				case '8':
+				{
+
+				}
+					break;
+				case '9':
+				{
+
+				}
+					break;
+				case '0':
+				{
+					cout << "\nYou are quitting the program!\n";
+					system("pause");
+				}
+					break;
+				default:
+				{
+					cout << "\nError! Please select a valid option:\n";
+				}
+					break;
+			}
+
+		} while (userEntryChoice != '0');
+		
+	}
+	catch (...)
+	{
+		cout << "Error!";
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
--- Assignment 1/Exercice_2.h
+++ Assignment 1/Exercice_2.h
@@ -1,20 +1,64 @@
 #pragma once
 class Exercice_2
-{ 
-	//Initialize variables
-	int nbrFloors{ 0 };
-	const int FIRST_FLOOR_HEIGHT{ 6 };
-	const int AVERAGE_FLOOR_HEIGHT{ 4 };
-
+{
 public:
-	//Declare function to calculate building height
+	////Declare method for the menu of the exercice
+	//int ExerciceMenu2()
+	//{
+	//	try
+	//	{
+	//		//Declare variable for user entry
+	//		int userEntry;
+
+
+	//	}
+	//	catch (...)
+	//	{
+
+	//	}
+	//}
+
+	//Declare method to calculate building height
 	int BuildingHeightCalculator(int userEntry)
 	{
-		//Set nrbFloors to the value of userEntry
-		nbrFloors = userEntry - 1;
+		try
+		{
+			//Initialize variables
+			int nbrFloors{ 0 };
+			const int FIRST_FLOOR_HEIGHT{ 6 };
+			const int AVERAGE_FLOOR_HEIGHT{ 4 };
 
-		//Return calculated height of the building
-		return FIRST_FLOOR_HEIGHT + (nbrFloors * AVERAGE_FLOOR_HEIGHT);
+			//Set nrbFloors to the value of userEntry
+			nbrFloors = userEntry - 1;
+
+			//Return calculated height of the building
+			return FIRST_FLOOR_HEIGHT + (nbrFloors * AVERAGE_FLOOR_HEIGHT);
+		}
+		catch (...)
+		{
+			
+		}
+	}
+
+	//Declare method to convert pressure
+	int AtmospherePressureConverter(float userEntry)
+	{
+		try
+		{
+			//Initialize variables
+			float kPaEntered;
+			const float ONE_ATM_IN_kPa = 101.325;
+
+			//Set kPaEntered to the value of userEntry
+			kPaEntered = userEntry;
+
+			//Return converted pressure from kPa to atm
+			return kPaEntered / ONE_ATM_IN_kPa;
+		}
+		catch (...)
+		{
+
+		}
 	}
 };
 
